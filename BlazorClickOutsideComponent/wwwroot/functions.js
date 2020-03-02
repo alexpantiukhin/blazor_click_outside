f42ea1d742b753b2061ab151ebd6e5d26259feef
Alex <pantjuxin-av@yandex.ru>
2020-03-02 19:10:50 +0300

    excluded ids

--- BlazorClickOutsideComponent/wwwroot/functions.js
+++ BlazorClickOutsideComponent/wwwroot/functions.js
@@ -2,10 +2,49 @@
 // wrapped in a .NET API
 
 window.OutClickComponent = {
-    setEvent: function (element, dotNet) {
+    elements: [],
+    setEvent: function (element, dotNet, excludedIDs) {
+
+        var findElement = this.elements.find(x => x.Id === element.Id);
+
+        if (!findElement) {
+            findElement = {
+                element: element,
+                excluded: []
+            };
+
+            if (excludedIDs) {
+                for (var i = 0; i < excludedIDs.length; i++) {
+                    var domElement = document.getElementById(excludedIDs[i]);
+                    if (domElement) {
+                        findElement.excluded.push(domElement);
+                    }
+                }
+            }
+
+            this.elements.push(findElement);
+        }
+
         window.addEventListener("click", (e) => {
             if (!element.contains(e.target)) {
-                dotNet.invokeMethodAsync("ClickOut");
+                for (var i = 0; i < this.elements.length; i++) {
+                    var findElement = this.elements[i];
+                    if (findElement.element === element) {
+                        var isOut = true;
+                        for (var j = 0; j < findElement.excluded.length; j++) {
+                            var excludedElement = findElement.excluded[j];
+
+                            if (excludedElement.contains(e.target)) {
+                                isOut = false;
+                                break;
+                            }
+                        }
+
+                        if (isOut) {
+                            dotNet.invokeMethodAsync("ClickOut");
+                        }
+                    }
+                }
             }
         });
     }
