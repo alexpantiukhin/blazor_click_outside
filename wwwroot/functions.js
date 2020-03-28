75b565072fc1fd6c58fe673122e73f6dd7c9cf56
Alex <Alex@DESKTOP-ENSNID7>
2020-03-28 11:02:51 +0300

    123

--- wwwroot/functions.js
+++ wwwroot/functions.js
@@ -6,39 +6,67 @@ window.OutClickComponent = {
     setExcludedElements: function (elements) {
         var outsideElement = elements[0];
 
-        for (var i = 0; i < this.excludedElements; i++) {
+        var hasOutside = false;
+        for (var x = 0; x < length; x++) {
             if (this.excludedElements[i].outsideElement === outsideElement) {
-
-                for (var j = 1; j < elements.length; j++) {
-                    if (!this.excludedElements[i].excluded) {
-                        this.excludedElements[i].excluded = [];
-                    }
-                    this.excludedElements[i].excluded.push(elements[j]);
-                }
+                hasOutside = true;
+                break;
             }
         }
 
+        if (!hasOutside) {
+            this.excludedElements.push({
+                outsideElement: outsideElement,
+                excluded: elements
+            });
+        }
+        else {
+            for (var i = 0; i < this.excludedElements; i++) {
+                if (this.excludedElements[i].outsideElement === outsideElement) {
+
+                    for (var j = 1; j < elements.length; j++) {
+                        if (!this.excludedElements[i].excluded) {
+                            this.excludedElements[i].excluded = [];
+                        }
+                        this.excludedElements[i].excluded.push(elements[j]);
+                    }
+                }
+            }
+        }
     },
+
     setEvent: function (dotNet, element) {
-        this.excludedElements.push({
-            outsideElement: element,
-            excluded: [
-                element
-            ]
-        });
+        var hasOutside = false;
+        for (var x = 0; x < this.excludedElements.length; x++) {
+            if (this.excludedElements[x].outsideElement === element) {
+                hasOutside = true;
+                break;
+            }
+        }
+
+        if (!hasOutside) {
+            this.excludedElements.push({
+                outsideElement: element,
+                excluded: [element]
+            });
+        }
 
         window.addEventListener("click", (e) => {
             for (var i = 0; i < this.excludedElements.length; i++) {
                 if (this.excludedElements[i].outsideElement === element) {
                     var excluded = this.excludedElements[i].excluded;
 
+                    var out = true;
                     for (var j = 0; j < excluded.length; j++) {
                         var currentElement = this.excludedElements[i].excluded[j];
 
-                        if (!currentElement.contains(e.target)) {
-                            dotNet.invokeMethodAsync("ClickOut");
-                            return;
+                        if (currentElement.contains(e.target)) {
+                            out = false;
+                            break;
                         }
+                    }
+                    if (out) {
+                        dotNet.invokeMethodAsync("ClickOut", element.id);
                     }
                     break;
                 }
