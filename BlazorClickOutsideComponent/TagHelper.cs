44d614a40b351dd6f346e9fbac86036d0c3917c8
Александр Пантюхин <alexander.pantuhin@libroplanta.com>
2020-06-10 10:50:21 +0300

    2.0.1

---
+++ BlazorClickOutsideComponent/TagHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Al.BlazorMultiDropDownList
+{
+    public static class TagHelper
+    {
+        public static HtmlString AddClickOutsideScripts(this IHtmlHelper html, string[] items)
+        {
+            string result = "<script src='_content/BlazorClickOutside/functions.js'></script>";
+            result += items == null ? null : string.Join("", items);
+            return new HtmlString(result);
+        }
+    }
+}
